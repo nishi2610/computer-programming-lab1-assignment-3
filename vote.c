d9ca0823af043c9c3210f927cbd21080ad9f7a7c
Nishi Shah <nishahh26@gmail.com>
2025-08-19 10:51:03 +0530

    Create vote.c

---
+++ vote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter your age:");
+scanf("%d",&a);
+if(a>=18){
+    printf("eligible to vote");
+    }
+else{
+    printf("not eligible to vote");}
+}
